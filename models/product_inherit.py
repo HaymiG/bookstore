ac86c187b17bbbc7794f87dfdebd2a91914973d0
haymi_g <haymig75@gmail.com>
2025-07-20 11:59:20 +0300

    edited product inherit.py

--- models/product_inherit.py
+++ models/product_inherit.py
@@ -1,6 +1,6 @@
-from odoo import models, fields
+# from odoo import models, fields
 
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+# class ProductTemplate(models.Model):
+#     _inherit = 'product.template'
 
-    is_bookstore_item = fields.Boolean(string="Bookstore Item")
+#     is_bookstore_item = fields.Boolean(string="Bookstore Item")
